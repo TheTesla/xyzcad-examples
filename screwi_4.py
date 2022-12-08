7eea7f2805f1b97cd18fd1960137640d49b24d0e
Stefan Helmert <stefan.helmert@t-online.de>
2022-12-08 20:52:15 +0100

    removed stl and gcode from project

---
+++ screwi_4.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from xyzcad import render
+from numba import jit
+import math
+import numpy as np
+
+@jit
+def screwprofile(x):
+    x = x / (2*math.pi)
+    return min(max(3*(x if x < 0.5 else 1-x), 0.3), 1.2)
+
+
+@jit
+def f(x,y,z):
+    l = 10
+    rg = 10
+    ra = 4
+    if z < 0:
+        return False
+    phi = math.atan2(y, x)
+    r = (x**2 + y**2)**0.5
+    if r * math.cos(((phi*180/math.pi) %60 -30)/180*math.pi) < ra:
+        return False
+    if z > l:
+        return False
+    ang = -math.atan2(y,x)
+    r = 2*screwprofile((4*(2*math.pi/6*1*z/4+ang+math.pi))%(2*math.pi)) + (x**2 + y**2)**0.5
+    if r < rg:
+        return True
+    return False
+
+render.renderAndSave(f, 'screwi_4.stl', 0.1)
+
