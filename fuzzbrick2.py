41622b90b81705649d903a9bcf20a19ca71c8d48
Stefan Helmert <stefan.helmert@t-online.de>
2023-12-10 22:37:07 +0100

    fuzzyometry cascading example

--- fuzzbrick2.py
+++ fuzzbrick2.py
@@ -40,6 +40,14 @@ def fuzzblockround(p,s):
     l = s[1]
     h = s[2]
     d = (min(w-x, w+x, 0)**2 + min(l-y, l+y, 0)**2 + min(h-z, h+z, 0)**2)**0.5
+    d += max(min(abs(x),w)-w, min(abs(y),l)-l, min(abs(z),h)-h)
+    return d
+
+@njit
+def fuzzblockround2(p,s):
+    x, y, z = p
+    w, l, h = s
+    d = ((min(abs(x),w))**0.5 + (min(abs(y),l))**0.5 + (min(abs(z),h))**0.5)**2
     return d
 
 @njit
@@ -58,19 +66,34 @@ def roundChamfer(a, b):
     a = max(a,0)
     b = max(b,0)
     #return a + b - (2*a*b)**0.5
-    return a + b + (2*a*b)**0.5
+    #return a + b + (2*a*b)**0.5 #correct
+    #return a + b + (a*b)**0.5
+    return (a**0.5 + b**0.5)**2
     #return max(a + b + (2*a*b)**0.5, a + b - (2*a*b)**0.5)
 
 @njit
 def f(x,y,z):
-    a = fuzzcylinderround((x,y,z),50,5)
-    b = fuzzblockround((x,y,z),(10,50,10))
-    c = fuzzblockround((x,y,z),(50,5,5))
+    a = fuzzcylinderround((x,y,z),70,1) - 5
+    b = fuzzblockround((x,y,z),(7,20,7)) - 3
+    c = fuzzblockround((x,y,z),(20,5,5)) - 3
+    d = fuzzblockround((x,y,z),(2,2,30)) -8
+    #d = 0
 
-    if 5 < roundChamfer(a,5-b) and (5 > b or 5 > c or 5 > roundChamfer(c-5,b-5)):
+    #if (min(0,b)**2 + min(0,c)**2 + min(0,a-5)**2)**0.5 > 3:
+    #rp = (max(0,b)**0.5 + max(0,c)**0.5 + max(0,d)**0.5)**2 -15
+    rp = (max(0,b)**0.5 + max(0,c)**0.5)**2 - 5
+    ##rl = (max(0,-c)**0.5 + max(0,-b)**0.5 + max(0,a)**0.5)**2 -6
+    rl = (max(0,rp+3)**2 + (-min(0,3+d))**2)**0.5 - 3 if d < -1 else -1
+    #rl = (rp+5)*abs(rp+5) + (d+5)*abs(d+5) - 97  if d < -1 else -1
+
+    #if a > -1000 and (0 > rp or 0 > b or 0 > c):
+    if rl < 0 and a > -100 and (0 > rp or 0 > b or 0 > c):
+
+    #if 5.01 < roundChamfer(a,10-b) and (10 > b or 10 > c or 10 > roundChamfer(c-5,b-5)):
+    #if 2 < fuzzblockround2((x,y,z),(10,50,10)):
         return True
 
     return False
 
-render.renderAndSave(f, f'fuzzbrick.stl', 0.5)
+render.renderAndSave(f, f'fuzzbrick.stl', 0.125)
 
